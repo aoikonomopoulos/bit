d4653f79c40b4ffe60f4bf554210277d90ba3f1d
Remco Vermeulen <rvermeulen@gmail.com>
2011-12-14 21:39:26 +0100

    Added basic test cases

---
+++ il/tests/ifelse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    if ( argc == 1 )
+    {
+        printf("if\n");
+    }
+    else
+    {
+        printf("else\n");
+    }
+    printf("merge point\n");
+    return 0;
+}
