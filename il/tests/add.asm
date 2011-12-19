b93ac53391fae454a39cb7b3385fce0e4a6d2681
Remco Vermeulen <rvermeulen@gmail.com>
2011-12-19 11:45:20 +0100

    Added test case
    Added case that add 2 16 bit registers

--- il/tests/add.asm
+++ il/tests/add.asm
@@ -1,3 +1,4 @@
+# Test cases for the ADD instruction
 BITS 32
 
 add eax,ebx
@@ -5,3 +6,5 @@ add dword [eax], ebx
 add dword [eax + 0x100], ebx
 add eax, [ebx]
 add dword [eax + ebx*2], ecx
+
+add ax,bx
