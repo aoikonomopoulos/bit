56ef70205393d1db307fb755082603ff1276fb37
Remco Vermeulen <r.vermeulen@vu.nl>
2012-01-04 16:04:04 +0100

    Changed the test case to test operand offset calculations.

--- il/tests/add.asm
+++ il/tests/add.asm
@@ -3,8 +3,10 @@ BITS 32
 
 add eax,ebx
 add dword [eax], ebx
+add eax, dword [ebx]
 add dword [eax + 0x100], ebx
-add eax, [ebx]
+add eax, dword [ebx + 0x100] 
 add dword [eax + ebx*2], ecx
-
-add ax,bx
+add eax, dword [ebx + ecx*2]
+add [0x100], eax
+add eax, [0x100]
