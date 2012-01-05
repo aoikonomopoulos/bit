11e8f826c1270c1673df817bb0d284f5a5381ef8
Remco Vermeulen <r.vermeulen@vu.nl>
2012-01-05 11:55:49 +0100

    Adjusted test cases for operand translation.

--- il/tests/mov.asm
+++ il/tests/mov.asm
@@ -3,6 +3,10 @@ BITS 32
 
 mov eax,ebx
 mov dword [eax], ebx
-mov dword [eax + 0x100], ebx
 mov eax, [ebx]
+mov dword [eax + 0x100], ebx
+mov eax, [ebx + 0x100]
 mov dword [eax + ebx*2], ecx
+mov eax, [ebx + ecx*2]
+mov dword [eax + ebx*2 + 0x100], ecx
+mov eax, [ebx + ecx*2 + 0x100]
