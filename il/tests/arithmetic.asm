e25ec7628fea0922d0f3b7559700d6b5a0891097
Remco Vermeulen <rvermeulen@gmail.com>
2012-01-06 13:08:25 +0100

    Merge add & sub tests into arithmetic test.

--- il/tests/arithmetic.asm
+++ il/tests/arithmetic.asm
@@ -12,3 +12,15 @@ add dword [eax + ebx*2], ecx
 add eax, dword [ebx + ecx*2]
 add [0x100], eax
 add eax, [0x100]
+
+sub eax, 0x100
+sub eax,ebx
+sub dword [eax], 0x100 
+sub dword [eax], ebx
+sub eax, dword [ebx]
+sub dword [eax + 0x100], ebx
+sub eax, dword [ebx + 0x100] 
+sub dword [eax + ebx*2], ecx
+sub eax, dword [ebx + ecx*2]
+sub [0x100], eax
+sub eax, [0x100]
