ee55a99a96a4df05d6612aa26b77d4f60880804d
Remco Vermeulen <r.vermeulen@vu.nl>
2012-01-17 09:23:44 +0100

    Fixed headers and made inline functions static.

--- il/x86/reil_x86.h
+++ il/x86/reil_x86.h
@@ -1,7 +1,7 @@
 #ifndef REIL_X86_H
 #define REIL_X86_h
-#include <libdasm.h>
-#include <opcode_tables.h>
-#include <../reil.h> 
-#include <reil_x86_translator.h>
+#include "../reil.h" 
+#include "libdasm.h"
+#include "opcode_tables.h"
+#include "reil_x86_translator.h"
 #endif
